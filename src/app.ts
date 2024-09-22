e940ca6076be5d9f276c721f4fbbeb18773e8620
mahmoodyasir <yasirmamun16@gmail.com>
2024-09-22 18:53:26 +0600

    file size increased for payload

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import router from './app/routes';
 
 const app: Application = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 
 app.use('/api', router);
